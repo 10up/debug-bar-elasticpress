f8eaffb36785ea8163bebcad07614e7a106baa73
Taylor Lovett <tlovett88@gmail.com>
2016-01-12 14:55:39 -0500

    Show query result and response code

--- classes/class-ep-debug-bar-elasticpress.php
+++ classes/class-ep-debug-bar-elasticpress.php
@@ -66,7 +66,14 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 			</ol>
 		<?php else : ?>
 			<ol class="wpd-queries">
-				<?php foreach ( $queries as $query ) : $query_time = ( ! empty( $query['time_start'] ) && ! empty( $query['time_finish'] ) ) ? $query['time_finish'] - $query['time_start'] : false; ?>
+				<?php foreach ( $queries as $query ) :
+					$query_time = ( ! empty( $query['time_start'] ) && ! empty( $query['time_finish'] ) ) ? $query['time_finish'] - $query['time_start'] : false;
+					$response = wp_remote_retrieve_response_code( $query['request'] );
+
+					if ( $response >= 200 && $response < 300 ) {
+						$result = wp_remote_retrieve_body( $query['request'] );
+					}
+					?>
 					<li>
 						<strong><?php esc_html_e( 'Host:', 'debug-bar'); ?></strong> <?php echo esc_html( $query['host'] ); ?><br>
 						<?php if ( ! empty( $query_time ) ) : ?>
@@ -77,6 +84,11 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 						<strong><?php esc_html_e( 'URL:', 'debug-bar'); ?></strong> <?php echo esc_url( $query['url'] ); ?><br>
 						<strong><?php esc_html_e( 'Method:', 'debug-bar'); ?></strong> <?php echo esc_html( $query['args']['method'] ); ?><br>
 						<strong><?php esc_html_e( 'Query Body:', 'debug-bar'); ?></strong> <pre><?php echo json_encode( json_decode( $query['args']['body'], true ), JSON_PRETTY_PRINT ); ?></pre><br>
+						<?php printf( __( '<strong>Query Response Code:</strong> HTTP %d', 'debug-bar'), (int) $response ); ?><br>
+						<?php if ( 200 <= $response && $response < 300 ) : ?>
+							<strong><?php esc_html_e( 'Query Result:', 'debug-bar'); ?></strong> <pre><?php echo json_encode( json_decode( $result, true ), JSON_PRETTY_PRINT ); ?></pre><br>
+						<?php endif; ?>
+
 					</li>
 				<?php endforeach; ?>
 			</ol>
