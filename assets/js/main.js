3d0a588b70e63d7c8b85725aaf69148b889cf878
Taylor Lovett <tlovett88@gmail.com>
2016-01-14 12:03:00 -0500

    Result/body toggles

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+( function( $ ) {
+
+	$( document ).ready( function() {
+		var queries = document.querySelectorAll( '.ep-queries-debug' )[0];
+
+		queries.addEventListener( 'click', function( event ) {
+			var queryWrapper = event.target;
+
+			while ( event.currentTarget.contains( queryWrapper ) ) {
+				if ( queryWrapper.nodeName === 'LI' ) {
+
+
+					if ( event.target.className.match( /query-body-toggle/i ) ) {
+						if ( queryWrapper.className.match( /hide-query-body/i ) ) {
+							queryWrapper.className = queryWrapper.className.replace( /hide-query-body/i, '' );
+						} else {
+							queryWrapper.className += ' hide-query-body';
+						}
+					}
+
+					if ( event.target.className.match( /query-result-toggle/i ) ) {
+						if ( queryWrapper.className.match( /hide-query-results/i ) ) {
+							queryWrapper.className = queryWrapper.className.replace( /hide-query-results/i, '' );
+						} else {
+							queryWrapper.className += ' hide-query-results';
+						}
+					}
+
+					break;
+				} else {
+					queryWrapper = queryWrapper.parentNode;
+				}
+			}
+
+		} );
+	} );
+
+})( jQuery );
--- classes/class-ep-debug-bar-elasticpress.php
+++ classes/class-ep-debug-bar-elasticpress.php
@@ -17,6 +17,17 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 	 */
 	public function setup() {
 		$this->title( esc_html__( 'ElasticPress', 'debug-bar' ) );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
+	}
+
+	/**
+	 * Enqueue scripts for front end and admin
+	 */
+	public function enqueue_scripts_styles() {
+		wp_enqueue_script( 'debug-bar-elasticpress', plugins_url( '../assets/js/main.js' , __FILE__ ), array( 'jquery' ), '1.0', true );
+		wp_enqueue_style( 'debug-bar-elasticpress', plugins_url( '../assets/css/main.css' , __FILE__ ), array(), '1.0' );
 	}
 
 	/**
@@ -65,7 +76,7 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 				<li><?php esc_html_e( 'No queries to show', 'debug-bar' ); ?></li>
 			</ol>
 		<?php else : ?>
-			<ol class="wpd-queries">
+			<ol class="wpd-queries ep-queries-debug">
 				<?php foreach ( $queries as $query ) :
 					$query_time = ( ! empty( $query['time_start'] ) && ! empty( $query['time_finish'] ) ) ? $query['time_finish'] - $query['time_start'] : false;
 					$response = wp_remote_retrieve_response_code( $query['request'] );
@@ -74,7 +85,7 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 						$result = wp_remote_retrieve_body( $query['request'] );
 					}
 					?>
-					<li>
+					<li class="ep-query-debug hide-query-body hide-query-results">
 						<strong><?php esc_html_e( 'Host:', 'debug-bar'); ?></strong> <?php echo esc_html( $query['host'] ); ?><br>
 						<?php if ( ! empty( $query_time ) ) : ?>
 							<?php printf( __( '<strong>Time Taken:</strong> %d ms', 'debug-bar'), ( $query_time * 1000 ) ); ?><br>
@@ -83,10 +94,10 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 						<?php endif; ?>
 						<strong><?php esc_html_e( 'URL:', 'debug-bar'); ?></strong> <?php echo esc_url( $query['url'] ); ?><br>
 						<strong><?php esc_html_e( 'Method:', 'debug-bar'); ?></strong> <?php echo esc_html( $query['args']['method'] ); ?><br>
-						<strong><?php esc_html_e( 'Query Body:', 'debug-bar'); ?></strong> <pre><?php echo json_encode( json_decode( $query['args']['body'], true ), JSON_PRETTY_PRINT ); ?></pre><br>
+						<strong><?php esc_html_e( 'Query Body:', 'debug-bar'); ?> <div class="query-body-toggle dashicons"></div></strong> <pre class="query-body"><?php echo json_encode( json_decode( $query['args']['body'], true ), JSON_PRETTY_PRINT ); ?></pre><br>
 						<?php printf( __( '<strong>Query Response Code:</strong> HTTP %d', 'debug-bar'), (int) $response ); ?><br>
 						<?php if ( 200 <= $response && $response < 300 ) : ?>
-							<strong><?php esc_html_e( 'Query Result:', 'debug-bar'); ?></strong> <pre><?php echo json_encode( json_decode( $result, true ), JSON_PRETTY_PRINT ); ?></pre><br>
+							<strong><?php esc_html_e( 'Query Result:', 'debug-bar'); ?> <div class="query-result-toggle dashicons"></div></strong> <pre class="query-results"><?php echo json_encode( json_decode( $result, true ), JSON_PRETTY_PRINT ); ?></pre><br>
 						<?php endif; ?>
 
 					</li>
