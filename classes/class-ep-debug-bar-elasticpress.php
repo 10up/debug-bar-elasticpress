7bb045c54d3a008956bd48891d30cd987c3fa756
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-01-29 19:04:25 +0200

    Improved readability

--- classes/class-ep-debug-bar-elasticpress.php
+++ classes/class-ep-debug-bar-elasticpress.php
@@ -102,10 +102,10 @@ class EP_Debug_Bar_ElasticPress extends Debug_Bar_Panel {
 						<?php endif; ?>
 						<strong><?php esc_html_e( 'URL:', 'debug-bar' ); ?></strong> <?php echo esc_url( $query['url'] ); ?><br>
 						<strong><?php esc_html_e( 'Method:', 'debug-bar' ); ?></strong> <?php echo esc_html( $query['args']['method'] ); ?><br>
-						<strong><?php esc_html_e( 'Query Body:', 'debug-bar' ); ?> <div class="query-body-toggle dashicons"></div></strong> <pre class="query-body"><?php echo json_encode( json_decode( $query['args']['body'], true ), JSON_PRETTY_PRINT ); ?></pre><br>
+						<strong><?php esc_html_e( 'Query Body:', 'debug-bar' ); ?> <div class="query-body-toggle dashicons"></div></strong> <pre class="query-body"><?php echo esc_html( stripslashes( json_encode( json_decode( $query['args']['body'], true ), JSON_PRETTY_PRINT ) ) ); ?></pre><br>
 						<?php printf( __( '<strong>Query Response Code:</strong> HTTP %d', 'debug-bar' ), (int) $response ); ?><br>
 						<?php if ( 200 <= $response && $response < 300 ) : ?>
-							<strong><?php esc_html_e( 'Query Result:', 'debug-bar' ); ?> <div class="query-result-toggle dashicons"></div></strong> <pre class="query-results"><?php echo json_encode( json_decode( $result, true ), JSON_PRETTY_PRINT ); ?></pre><br>
+							<strong><?php esc_html_e( 'Query Result:', 'debug-bar' ); ?> <div class="query-result-toggle dashicons"></div></strong> <pre class="query-results"><?php echo esc_html( stripslashes( json_encode( json_decode( $result, true ), JSON_PRETTY_PRINT ) ) ); ?></pre><br>
 						<?php endif; ?>
 
 					</li>
