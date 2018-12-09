78e1925b75c4daaabc2a8c4b4f6896e2bccc5e75
Oscar Sanchez <oscar.sanchez@10up.com>
2018-12-08 22:20:06 -0600

    Display ElasticPress/ElasticSearch version.
    Display ES and EP version on debug bar panel.

--- debug-bar-elasticpress.php
+++ debug-bar-elasticpress.php
@@ -25,6 +25,27 @@ function ep_add_debug_bar_panel( $panels ) {
 add_filter( 'debug_bar_panels', 'ep_add_debug_bar_panel' );
 
 /**
+ * Registers EP and ES versions on panel
+ *
+ * @param $statuses
+ * @return array
+ */
+function ep_add_debug_bar_status( $statuses ) {
+	$statuses[] = array(
+		'ep_version',
+		__( 'ElasticPress Version', 'debug-bar' ),
+		get_option( 'ep_version' ),
+	);
+	$statuses[] = array(
+		'es_version',
+		__( 'ElasticSearch Version', 'debug-bar' ),
+		ep_get_elasticsearch_version(),
+	);
+	return $statuses;
+}
+add_filter( 'debug_bar_statuses', 'ep_add_debug_bar_status' );
+
+/**
  * Add explain=true to elastic post query
  *
  * @param array $formatted_args
