48700fbb7515659dc7ff3c790184e5eb2b50dac8
mohammed.razzaq <mohammed.razzaq@get10up.com>
2023-03-14 18:40:23 +0530

    Use already present code

--- classes/class-ep-debug-bar-queries-info.php
+++ classes/class-ep-debug-bar-queries-info.php
@@ -65,9 +65,12 @@ class QueriesInfo extends \ElasticPress\StatusReport\Report {
 
 		$groups = [];
 		foreach ( $this->queries as $query ) {
-			$query = $this->format_log_entry( $query, 'query' );
+			/* this filter is documented in elasticpress.php */
+			$query_logger   = apply_filters( 'ep_query_logger', new QueryLogger() );
+			$failed_queries = new \ElasticPress\StatusReport\FailedQueries( $query_logger );
+			$query          = $this->format_log_entry( $query, 'query' );
 
-			list( $error, $solution ) = $this->analyze_log( $query );
+			list( $error, $solution ) = $failed_queries->analyze_log( $query );
 			if ( ! empty( $error ) ) {
 				$fields = [
 					'error'                => [
@@ -148,98 +151,5 @@ class QueriesInfo extends \ElasticPress\StatusReport\Report {
 		];
 
 		return $formatted_log;
-	}
-
-	/**
-	 * Given a log, try to find the error and its solution
-	 *
-	 * @param array $log The log
-	 * @return array The error in index 0, solution in index 1
-	 */
-	public function analyze_log( $log ) {
-		$error = '';
-
-		if ( ! empty( $log['result']['error'] ) && ! empty( $log['result']['error']['root_cause'][0]['reason'] ) ) {
-			$error = $log['result']['error']['root_cause'][0]['reason'];
-		}
-
-		if ( ! empty( $log['result']['errors'] ) && ! empty( $log['result']['items'] ) && ! empty( $log['result']['items'][0]['index']['error']['reason'] ) ) {
-			$error = $log['result']['items'][0]['index']['error']['reason'];
-		}
-
-		$solution = ( ! empty( $error ) ) ?
-			$this->maybe_suggest_solution_for_es( $error ) :
-			'';
-
-		return [ $error, $solution ];
-	}
-
-	/**
-	 * Given an Elasticsearch error, try to suggest a solution
-	 *
-	 * @param string $error The error
-	 * @return string
-	 */
-	protected function maybe_suggest_solution_for_es( $error ) {
-		$sync_url = Utils\get_sync_url();
-
-		if ( preg_match( '/no such index \[(.*?)\]/', $error, $matches ) ) {
-			return sprintf(
-				/* translators: 1. Index name; 2. Sync Page URL */
-				__( 'It seems the %1$s index is missing. <a href="%2$s">Delete all data and sync</a> to fix the issue.', 'debug-bar-elasticpress' ),
-				'<code>' . $matches[1] . '</code>',
-				$sync_url
-			);
-		}
-
-		if ( preg_match( '/No mapping found for \[(.*?)\] in order to sort on/', $error, $matches ) ) {
-			return sprintf(
-				/* translators: 1. Index name; 2. Sync Page URL */
-				__( 'The field %1$s was not found. Make sure it is added to the list of indexed fields and run <a href="%2$s">a new sync</a> to fix the issue.', 'debug-bar-elasticpress' ),
-				'<code>' . $matches[1] . '</code>',
-				$sync_url
-			);
-		}
-
-		/* translators: 1. Field name; 2. Sync Page URL */
-		$field_type_solution = __( 'It seems you saved a post without doing a full sync first because <code>%1$s</code> is missing the correct mapping type. <a href="%2$s">Delete all data and sync</a> to fix the issue.', 'debug-bar-elasticpress' );
-
-		if ( preg_match( '/Fielddata is disabled on text fields by default. Set fielddata=true on \[(.*?)\]/', $error, $matches ) ) {
-			return sprintf( $field_type_solution, $matches[1], $sync_url );
-		}
-
-		if ( preg_match( '/field \[(.*?)\] is of type \[(.*?)\], but only numeric types are supported./', $error, $matches ) ) {
-			return sprintf( $field_type_solution, $matches[1], $sync_url );
-		}
-
-		if ( preg_match( '/Alternatively, set fielddata=true on \[(.*?)\] in order to load field data by uninverting the inverted index./', $error, $matches ) ) {
-			return sprintf( $field_type_solution, $matches[1], $sync_url );
-		}
-
-		if ( preg_match( '/Limit of total fields \[(.*?)\] in index \[(.*?)\] has been exceeded/', $error, $matches ) ) {
-			return sprintf(
-				/* translators: Elasticsearch or ElasticPress.io; 2. Link to article; 3. Link to article */
-				__( 'Your website content has more public custom fields than %1$s is able to store. Check our articles about <a href="%2$s">Elasticsearch field limitations</a> and <a href="%3$s">how to index just the custom fields you need</a> and sync again.', 'debug-bar-elasticpress' ),
-				Utils\is_epio() ? __( 'ElasticPress.io', 'debug-bar-elasticpress' ) : __( 'Elasticsearch', 'debug-bar-elasticpress' ),
-				'https://elasticpress.zendesk.com/hc/en-us/articles/360051401212-I-get-the-error-Limit-of-total-fields-in-index-has-been-exceeded-',
-				'https://elasticpress.zendesk.com/hc/en-us/articles/360052019111'
-			);
-		}
-
-		// field limit
-
-		if ( Utils\is_epio() ) {
-			return sprintf(
-				/* translators: ElasticPress.io My Account URL */
-				__( 'We did not recognize this error. Please open an ElasticPress.io <a href="%s">support ticket</a> so we can troubleshoot further.', 'debug-bar-elasticpress' ),
-				'https://www.elasticpress.io/my-account/'
-			);
-		}
-
-		return sprintf(
-			/* translators: New GitHub issue URL */
-			__( 'We did not recognize this error. Please consider opening a <a href="%s">GitHub Issue</a> so we can add it to our list of supported errors. ', 'debug-bar-elasticpress' ),
-			'https://github.com/10up/ElasticPress/issues/new/choose'
-		);
 	}
 }
